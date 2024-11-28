21c67acaabe9250ff165719108542360a80a20a0
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2024-11-28 09:20:25 +0200

    DeskMarket_ProjectAdded

---
+++ DeskMarket/DeskMarket/Common/ValidationConstants.cs
@@ -0,0 +1,19 @@
+﻿namespace DeskMarket.Common
+{
+    public static class ValidationConstants
+    {
+        //Category
+        public const int CategoryNameMinLength = 3;
+        public const int CategoryNameMaxLength = 20;
+
+        //Product
+        public const int ProductNameMinLength = 2;
+        public const int ProductNameMaxLength = 60;
+
+        public const int ProductDescriptionMinLength = 10;
+        public const int ProductDescriptionMaxLength = 250;
+
+        public const string ProductPriceMinValue = "1.00";
+        public const string ProductPriceMaxValue = "3000.00";
+    }
+}
---
+++ DeskMarket/DeskMarket/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeskMarket.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //base.OnModelCreating(builder);
+
+            //builder
+            //    .Entity<Category>()
+            //    .HasData(
+            //        new Category { Name = "Laptops" },
+            //        new Category { Name = "Workstations" },
+            //        new Category { Name = "Accessories" },
+            //        new Category { Name = "Desktops" },
+            //        new Category { Name = "Monitors" });
+        }
+    }
+}
