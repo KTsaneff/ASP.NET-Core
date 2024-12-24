16dc3749e8526c18c97bf69fa6a4cfbc01c6445a
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2024-12-24 14:26:58 +0200

    IdentityUser-Extended

---
+++ LoopSocialApp/LoopSocialApp.Data/Migrations/20241224120505_Changed-Post-Id.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoopSocialApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedPostId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PostId",
+                table: "Posts",
+                newName: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Posts",
+                newName: "PostId");
+        }
+    }
+}
